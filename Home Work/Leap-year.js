9ce8d813ad21434eff039571ba7b14c925c0d859
AshrafUzMahim <ashraf.uzzaman04082004@gmail.com>
2022-08-21 20:41:28 +0600

    last commit on hwe

--- Home Work/Leap-year.js
+++ Home Work/Leap-year.js
@@ -1,14 +1,16 @@
 // Leap-year function given below
 
 function year(x) {
-  if ((0 == x % 4 && 0 != x % 100) || 0 == x % 400) {
-    document.write(x + " " + "is a Leap-year.");
-  } else if (x <= 0) {
+  if (x < 0 || isNaN(x) || x % 1 !== 0) {
     document.write(
       "<h1>To know which year is leap year, please enter any integer</h1>"
     );
   } else {
-    document.write(x + " " + "is not a Leap-year.");
+    if ((x % 4 == 0 && x % 100 != 0) || x % 400 == 0) {
+      document.write(x + " " + "is a Leap-year.");
+    } else {
+      document.write(x + " " + "is not a Leap-year.");
+    }
   }
 }
 
